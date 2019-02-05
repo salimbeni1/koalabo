8b8dc743f90f9b8bb6dcff8f18049f1d763d9044
salimbeni1 <32538308+salimbeni1@users.noreply.github.com>
2019-02-05 17:15:20 +0100

    new fonts

--- public/scripts/mathlinker.js
+++ public/scripts/mathlinker.js
@@ -1,9 +1,13 @@
 var body = document.body;
 body.style.cssText = 
+    "background-color : #D5A64E";
+
+var eng = document.createElement("div")
+eng.style.cssText = 
     "display : flex;"
     +"flex-direction : row;"
-    +"justify-content : space-between;";
-
+    +"justify-content : space-around;"
+    +"background-color : trasparent";
 
 function makelink(text, link) {
     var fr2 = document.createElement("a");
@@ -11,13 +15,23 @@ function makelink(text, link) {
     fr2.innerHTML =  text ;
     fr2.style.cssText =
         "display : block;"
-        +"height : 100px;"
-        +"width : 100px;"
-        +"background-color : red;"
+        +"height : auto;"
+        +"width : auto;"
+        +"background-color : #12662F;"
+        +"border-radius : 10px;"
+        +"text-align : center;"
+        +"font-size : 100px;"
+        +"padding : 20px;"
+        +"border-style: solid;"
+        +"color : black;"
+        +"border-width: 5px;"
+        +"margin-top : 10%;"
  return fr2   
 }
 
 
-body.append(makelink("2fr","math2fr.html"),
+eng.append(makelink("2fr","math2fr.html"),
            makelink("3fr","math3fr.html"),
            makelink("4fr","math4fr.html"));
+
+body.append(eng);
