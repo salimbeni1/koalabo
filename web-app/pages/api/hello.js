337bb75d9108ba1172dd80fee6a0c6a99fd16205
salimbeni1 <etienne.salimbeni@epfl.ch>
2021-12-12 23:49:20 +0100

    push test

--- web-app/pages/api/hello.js
+++ web-app/pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'NO API SERVICE' })
+  res.status(200).json({ name: 'NO API SERVIC E' })
 }
